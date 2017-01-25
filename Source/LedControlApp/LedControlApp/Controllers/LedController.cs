8ef1e8cac6bf8d994bc12dd185f3258e315b764b
Садовский Антон Дмитриевич <mn0016@coins-business.local>
2017-01-25 17:12:26 +0700

    Added implementations

--- Source/LedControlApp/LedControlApp/Controllers/LedController.cs
+++ Source/LedControlApp/LedControlApp/Controllers/LedController.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Plugin.BLE.Abstractions.Contracts;
+using Plugin.BLE.Abstractions.EventArgs;
 
 namespace LedControlApp.Controllers
 {
     public class LedController
     {
-        private IAdapter _adapter;
+        private Guid _serviceGuid = Guid.Parse("0000fff0-0000-1000-8000-00805f9b34fb");
+        private Guid _charGuid = Guid.Parse("0000fff1-0000-1000-8000-00805f9b34fb");
 
+        private IAdapter _adapter;
+        private IDevice _device;
         public LedController(IAdapter adapter)
         {
             _adapter = adapter;
@@ -18,10 +23,39 @@ namespace LedControlApp.Controllers
 
         public async Task<byte?> GetLedValue()
         {
-            return byte.MaxValue;
+            var service = await _device.GetServiceAsync(_serviceGuid);
+            var charact = await service.GetCharacteristicAsync(_charGuid);
+            return (await charact.ReadAsync()).First();
         }
 
         public async Task<bool> SetLedValue(byte value)
+        {
+            var service = await _device.GetServiceAsync(_serviceGuid);
+            var charact = await service.GetCharacteristicAsync(_charGuid);
+            return await charact.WriteAsync(new byte[] {value});
+        }
+
+        public async Task<bool> FindAndConnectToDevice()
+        {
+            _adapter.ScanTimeout = 10;
+            await _adapter.StartScanningForDevicesAsync();
+            var device =_adapter.DiscoveredDevices.FirstOrDefault(x => FilterDevice(x));
+            if (device != null && await ConnectToDevice(device))
+            {
+                _device = device;
+            }
+            
+            return _device != null;
+        }
+
+       
+
+        private bool FilterDevice(IDevice device)
+        {
+            return device.Name.ToLower().Contains("phito");
+        }
+
+        private async Task<bool> ConnectToDevice(IDevice device)
         {
             return true;
         }
