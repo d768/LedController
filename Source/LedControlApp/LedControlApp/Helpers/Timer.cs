4f778ea06f3e5606574e853908773f199b0366d4
Садовский Антон Дмитриевич <mn0016@coins-business.local>
2017-01-25 17:13:08 +0700

    Added Timer Added VMBase class Added implementation to vm

---
+++ Source/LedControlApp/LedControlApp/Helpers/Timer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LedControlApp.Helpers
+{
+    public class Timer
+    {
+        private bool _isPolling = false;
+        private TimeSpan _timeout;
+        private Func<object,Task> _action;
+        private object _state;
+
+        public Timer(object state, TimeSpan timeout, Func<object, Task> action)
+        {
+            _state = state;
+            _timeout = timeout;
+            _action = action;
+        }
+        public async Task StartTimer()
+        {
+            _isPolling = true;
+            while (_isPolling)
+            {
+                await Task.Delay(_timeout);
+                await _action(_state);
+            }
+        }
+
+        public void StopTimer()
+        {
+            _isPolling = false;
+        }
+    }
+}
--- Source/LedControlApp/LedControlApp/ViewModels/LedControllerPageViewModel.cs
+++ Source/LedControlApp/LedControlApp/ViewModels/LedControllerPageViewModel.cs
@@ -5,18 +5,59 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 using LedControlApp.Controllers;
+using LedControlApp.Helpers;
+using Xamarin.Forms;
 
 namespace LedControlApp.ViewModels
 {
-    public class LedControllerPageViewModel
+    public class LedControllerPageViewModel:ViewModelBase
     {
         private Timer _timer;
         private LedController _ledController;
-        public byte LedIntensity { get; set; } = 0;
 
+        public byte LedIntensity { get; set; } = 0;
+        public string Log { get; set; } = "log";
+        public Command ConnectCommand { get; set; }
         public LedControllerPageViewModel(LedController controller)
         {
             _ledController = controller;
+            ConnectCommand = new Command(ConnectCommandExecute);
+            
+        }
+
+        private async void ConnectCommandExecute()
+        {
+            try
+            {
+
+                var isConnected = false;
+                while (!isConnected)
+                {
+                    isConnected = _ledController.FindAndConnectToDevice().Result;
+                }
+
+                _timer = new Timer(null, TimeSpan.FromSeconds(3), WriteValueTo);
+                _timer.StartTimer();
+            }
+            catch (AggregateException ex)
+            {
+
+                var fEx = ex.Flatten();
+                foreach (var e in fEx.InnerExceptions)
+                {
+                    throw e;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        private async Task WriteValueTo(object state)
+        {
+            await _ledController.SetLedValue(LedIntensity);
         }
 
     }
